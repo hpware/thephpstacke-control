470f83a3236d2e886cc4c05273a9ca2c950df84d
元皓yh <hw@yuanhau.com>
2025-02-12 12:16:15 +0800

    Create head.php

---
+++ head.php
@@ -0,0 +1,5 @@
+<title>The Control Stack</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="robots" content="index, nofollow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
